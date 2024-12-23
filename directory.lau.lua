fefaf8ceca7b52ea4aa1e775a2221b008a37a22c
akindaG <akindagunarathne@gmail.com>
2024-12-23 23:22:16 +0530

    Add files via upload

---
+++ directory.lau.lua
@@ -0,0 +1,6 @@
+/coding-competition
+|-- index.html         (Home Page)
+|-- timeline.html      (Timeline Page)
+|-- shop.html          (Shop Page)
+|-- contact.html       (Contact Us Page)
+|-- style.css          (Shared CSS File)
